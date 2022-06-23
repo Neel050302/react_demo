f14c237a424ad5dbdaa01dff7291a065b96f7602
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2022-06-23 06:52:24 +0530

    SIMPLE DEMO WEBSITE FOR MY FAV SERIES NAME

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<><h1>Neel</h1><h2>Deakin university</h2></>
+ReactDOM.render(<>
+<h1>Neel Lodhiya's fav 5 series</h1>
+<p>My List: </p>
+<ol>
+  <li>DARK</li>
+  <li>VAMPIRE DARIES</li>
+  <li>MONEY HEIST</li>
+  <li>LUCIFER</li>
+  <li>PEAKY BLINDER</li>
+</ol>
+</>
 
 , document.getElementById("root"));
