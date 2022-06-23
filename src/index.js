cb197864f8094e4b374ca3d94ef78ce38b64e75e
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2022-06-23 06:33:49 +0530

    how to use react and react Dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>neel lodhiya</h1>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<h1>Neel Vishal Lodhiya</h1>, document.getElementById("root"));
