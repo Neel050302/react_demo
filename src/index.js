9986944ce52608b82dd77f2bdcdcb368743f678e
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2022-06-23 06:47:06 +0530

    React fragment and its syntetic sugarform

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
-  [
-    <h1>Neel Vishal Lodhiya</h1>,
-    <h2>Deakin University</h2>
-  ]
+ReactDOM.render(<><h1>Neel</h1><h2>Deakin university</h2></>
 
 , document.getElementById("root"));
