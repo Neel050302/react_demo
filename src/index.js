7a3feece3b8a7aa6fef8b954d56b69397636cc08
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2022-06-23 06:41:21 +0530

    add multiple element using div

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<h1>Neel Vishal Lodhiya</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Neel Vishal Lodhiya</h1>
+    <h2>Deakin University</h2>
+  </div>
+
+, document.getElementById("root"));
