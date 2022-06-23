ab4499d238a1aa30a7a4870b48b5581c9003c0e3
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2022-06-23 06:41:54 +0530

    add multiple element using array of element

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  <div>
-    <h1>Neel Vishal Lodhiya</h1>
+  [
+    <h1>Neel Vishal Lodhiya</h1>,
     <h2>Deakin University</h2>
-  </div>
+  ]
 
 , document.getElementById("root"));
