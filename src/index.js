9ab4f9cf8aee39f7e11adde6ac7aa83b621394b3
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2022-06-27 14:20:25 +0530

    add images

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ const time = new Date().toLocaleTimeString();
 /* only use expression not use statement */
 
 ReactDOM.render(<>
-  <h1>{`My name is ${fname}  ${lname}`} </h1>
+  <h1 contentEditable="true">{`My name is ${fname}  ${lname}`} </h1>
   <p>{`Today's Date: ${date} `}</p>
   <p>{`Time: ${time}`}</p>
+  <img src="https://picsum.photos/210/300"  alt="img"/>
+  <img src="https://picsum.photos/220/300"  alt="img"/>
+  <img src="https://picsum.photos/230/300"  alt="img"/>
 </>
 
 , document.getElementById("root"));
