7b14a761838bd5f05f11231969b364dad1888d3d
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2022-07-01 10:00:24 +0530

    greeting project 1

--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const fname = "neel";
-const lname= "lodhiya";
+
 const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
-/* only use expression not use statement */
+const time = new Date().getHours();
+let greeting = '';
+const cssStyle = { };
 
-
-
-
+if(time >=1 && time<12){
+  greeting ='good morning';
+  cssStyle.color = 'green';
+}else if(time>=12 && time<20){
+  greeting ='good afternoon';
+}else{
+  greeting ='good evening';
+}
 ReactDOM.render(<>
-<div >
-  <h1 style={{fontStyle: 'italic',
-  color: 'blue',
-  textAlign: 'center'}}>{`My name is ${fname}  ${lname}`} </h1>
+<div className='main'>
+  <div className='header'>
+   <h1>Hello sir,<span style={cssStyle}> {greeting}</span></h1>
+  </div>
 </div>
-  
-<div className='image_div'>
-<img src="https://picsum.photos/210/300" className='image_div'alt="img"/>
-  <img src="https://picsum.photos/220/300" className='image_div' alt="img"/>
-  <img src="https://picsum.photos/230/300" className='image_div' alt="img"/>
-</div>
+
 
 </>
 
